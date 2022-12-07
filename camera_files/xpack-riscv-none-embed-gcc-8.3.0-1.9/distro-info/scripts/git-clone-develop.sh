403f93bdedf353fbf359abdf157879131888f5ed
unknown <yansizhe2019@163.com>
2022-12-06 16:00:02 -0800

    remove camera files

--- camera_files/xpack-riscv-none-embed-gcc-8.3.0-1.9/distro-info/scripts/git-clone-develop.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-rm -rf "${HOME}/Downloads/riscv-none-embed-gcc-xpack.git"
-git clone --branch develop --recurse-submodules https://github.com/xpack-dev-tools/riscv-none-embed-gcc-xpack.git "${HOME}/Downloads/riscv-none-embed-gcc-xpack.git"
