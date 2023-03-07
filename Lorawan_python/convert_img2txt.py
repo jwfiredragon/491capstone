7b54487d2bf0d5213728b4afc719029b5ed54fae
nicolecam <niccam18@gmail.com>
2023-03-07 13:26:35 -0800

    Update convert_img2txt.py

--- Lorawan_python/convert_img2txt.py
+++ Lorawan_python/convert_img2txt.py
@@ -1,6 +1,10 @@
-# Untitled - By: ncamp - Thu Mar 2 2023
+# "convert_img2txt"
+# By: N. Campbell - Thu Mar 2 2023
+# This program takes an image, converts it to text using b64encode,
+# and prints the converted string to the command line.
 
 import sensor, image
+from base64 import b64encode
 
 sensor.reset() # Initialize the camera sensor.
 sensor.set_pixformat(sensor.GRAYSCALE) # or sensor.GRAYSCALE
@@ -10,8 +14,8 @@ sensor.skip_frames(time = 2000) # Let new settings take affect.
 sensor.skip_frames(time = 2000) # Give the user time to get ready.
 
 print("Taking snapshot!")
-sensor.snapshot().save("example.jpg") # or "example.bmp" (or others)
+sensor.snapshot().save("example.jpg")
 
-write_str = base64.b64encode("example.jpg")
-print("Example: /n/n")
+write_str = base64.b64encode("example.jpg") # encode image
+print("Example image encoded with base64: \n\n")
 print(write_str)
