3d69e87313a67ebc9b87fb9c0b68a58a41e864cd
nicolecam <niccam18@gmail.com>
2023-03-07 13:26:38 -0800

    Update LoRaWAN_program.py

--- Lorawan_python/LoRaWAN_program.py
+++ Lorawan_python/LoRaWAN_program.py
@@ -7,22 +7,20 @@
 # Rxd -> pin 33
 # Txd -> pin 34
 # AUX -> pin 35
-# Vcc -> 5V
+# Vcc -> 3.3V
 # Gnd -> Gnd
 
 from fpioa_manager import fm
 from machine import UART
-
 from time import sleep_ms
-
 import sensor, image
 
 # Setup
 fm.register(34,fm.fpioa.UART1_TX)
-fm.register(35,fm.fpioa.UART1_RX)
+fm.register(33,fm.fpioa.UART1_RX)
 
 uart_A = UART(UART.UART1, 9600, 34, None, 1, timeout=1000, read_buf_len=4096)
-uart_B = UART(UART.UART2, 9600, 35, None, 1, timeout=1000, read_buf_len=4096)
+uart_B = UART(UART.UART2, 9600, 33, None, 1, timeout=1000, read_buf_len=4096)
 
 
 
@@ -35,16 +33,16 @@ sensor.skip_frames(time = 2000) # Let new settings take affect.
 
 sensor.skip_frames(time = 2000) # Give the user time to get ready.
 
-print("Taking a picture")
+println("Taking a picture.")
 sensor.snapshot().save("example.jpg") # or "example.bmp" (or others)
 
-print("Done! Reset the camera to see the saved image.")
+println("Finished taking picture.")
 
 
 # Writing
 write_str = base64.b64encode("example.jpg")
-for i in range(len(write_str)/500): #send 500 bytes at a time
-    uart_A.write(write_str[i*500:i*500+1])
+for i in range(floor(len(write_str)/500)): #send 500 bytes at a time
+    uart_A.write(write_str[i*500:i*500+501])
     time.sleep_ms(10)
 
 sleep_ms(500)
