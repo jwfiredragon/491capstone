661a56b3729a76de2b6364c9b98dd696a9b3a5f1
nicolecam <niccam18@gmail.com>
2023-03-10 12:22:28 -0800

    Success! Maix board reads and writes over lora
    The program currently sends a pre-programmed string to the arduino a few times (arduino receives it). Then it goes into a while loop and reads any serial data sent from the Arduino.

--- Lorawan_python/LoRaWAN_program.py
+++ Lorawan_python/LoRaWAN_program.py
@@ -14,41 +14,65 @@ from fpioa_manager import fm
 from machine import UART
 from time import sleep_ms
 import sensor, image
+import ubinascii
 
 # Setup
-fm.register(34,fm.fpioa.UART1_TX)
-fm.register(33,fm.fpioa.UART1_RX)
-
-uart_A = UART(UART.UART1, 9600, 34, None, 1, timeout=1000, read_buf_len=4096)
-uart_B = UART(UART.UART2, 9600, 33, None, 1, timeout=1000, read_buf_len=4096)
+fm.register(33,fm.fpioa.UART1_TX) #note that Uart1_TX connects to Rxd
+fm.register(34,fm.fpioa.UART1_RX)
 
 
+uart_A = UART(UART.UART1, 9600, 8, None, 1, timeout=1000, read_buf_len=4096)
 
-# take picture
+## take picture
 
-sensor.reset() # Initialize the camera sensor.
-sensor.set_pixformat(sensor.GRAYSCALE)
-sensor.set_framesize(sensor.QVGA) # or sensor.QQVGA (or others)
-sensor.skip_frames(time = 2000) # Let new settings take affect.
+#sensor.reset() # Initialize the camera sensor.
+#sensor.set_pixformat(sensor.GRAYSCALE)
+#sensor.set_framesize(sensor.QVGA) # or sensor.QQVGA (or others)
+#sensor.skip_frames(time = 2000) # Let new settings take affect.
 
-sensor.skip_frames(time = 2000) # Give the user time to get ready.
+#sensor.skip_frames(time = 2000) # Give the user time to get ready.
 
-println("Taking a picture.")
-sensor.snapshot().save("example.jpg") # or "example.bmp" (or others)
-
-println("Finished taking picture.")
+#print("Taking a picture.")
+#image = sensor.snapshot()
 
 
-# Writing
-write_str = base64.b64encode("example.jpg")
-for i in range(floor(len(write_str)/500)): #send 500 bytes at a time
-    uart_A.write(write_str[i*500:i*500+501])
-    time.sleep_ms(10)
+#ibytes = ubinascii.b2a_base64(image)
+#istr = str(ibytes)[2:-3]
+
+
+# ~~~~~ Writing ~~~~~
+
+#write_str = base64.b64encode("example.jpg")
+#for i in range(floor(len(write_str)/500)): #send 500 bytes at a time
+    #uart_A.write(write_str[i*500:i*500+501])
+    #time.sleep_ms(10)
+
+write_str = "Example. "
+for i in range(10):
+    uart_A.write(write_str)
+    sleep_ms(20)
 
 sleep_ms(500)
 
 
-# Reading
-read_data = uart_B.read()
-read_str = read_data.decode('utf-8')
-print("string = ",read_str)
+# ~~~~~ Reading ~~~~~
+#for i in range(60):
+while(True):
+
+    if uart_A.any():
+        read_data = uart_A.read()
+        read_str = read_data.decode('utf-8')
+        print("string = ",read_str)
+    #else:
+        #print("not available")
+
+    sleep_ms(10)
+
+
+# clean up
+uart_A.deinit()
+del uart_A
+
+
+
+# reference: https://wiki.sipeed.com/soft/maixpy/en/api_reference/machine/uart.html
