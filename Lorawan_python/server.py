359c1a4af789611b6697274c1f83e617c862fc2d
jwfiredragon <jwfiredragon@gmail.com>
2023-04-06 21:54:21 -0700

    preprocessing, and receive device id + timestamp

--- Lorawan_python/server.py
+++ Lorawan_python/server.py
@@ -1,7 +1,7 @@
 import binascii
 import io
 import pytesseract
-from PIL import Image
+from PIL import Image, ImageFilter, ImageEnhance
 from serial import Serial
 
 ser = Serial('COM5', 9800, timeout=1)
@@ -17,16 +17,33 @@ while True:
         line = ser.readline()
         print('...')
         if line is not b'':
-            # hack for buffer character dropping
+            # Workaround for buffer dropping characters
+            # Removes leading/trailing 'x's (ASCII code 120) if present
             if line[0] == 120:
                 line = line[1:]
             if line[-1] == 120:
                 line = line[:-2]
 
-            ibytes = binascii.unhexlify(line)
+            # msg[0] = device ID, msg[1] = timestamp, msg[2] = image bytes
+            msg = line.split(' ')
+
+            # Convert the received message into a JPG image
+            ibytes = binascii.unhexlify(msg[2])
             image = Image.open(io.BytesIO(ibytes))
-            image.save('./ser_out.jpg')
-            text = pytesseract.image_to_string('ser_out.jpg', lang='lets')
+
+            # preprocess image to join disconnected line segments for more consistent OCR
+            # small radius Gaussian blur, then conversion to pure black and white
+            image = image.filter(ImageFilter.GaussianBlur(radius=2))
+            fn = lambda x : 255 if x > 90 else 0
+            image = image.convert('L').point(fn, mode='1')
+
+            image.save('./ser_out.bmp')
+
+            # Send receipt acknowledgement to device
             ser.write('Image received'.encode())
-            print(f'Image received. Text: {text}')
+
+            # Perform OCR on image and print result
+            text = pytesseract.image_to_string('ser_out.bmp', lang='lets',  config='-c tessedit_char_whitelist=0123456789')
+            print(f'Reading from ID {msg[0]} at {msg[1]}: {text}')
+            
             img_received = True
